9d2bb1a696beec89e8eda00f2eae195d6d915fc4
“austinogola” <austinandogola@gmail.com>
2024-01-24 21:35:43 +0300

    rmv Dep, Funds and Opt

--- extension/content/views.js
+++ extension/content/views.js
@@ -2,7 +2,6 @@
 
 const modifyAccountInfo=async()=>{
     let signOutSpan=await loadSelector('span:contains("Account Settings")')
-    // console.log(signOutSpan);
     let parentElement=signOutSpan.parentNode
 
     let sibling  = parentElement.firstChild;
@@ -10,39 +9,61 @@ const modifyAccountInfo=async()=>{
         let text=sibling.textContent.toLowerCase()
         if(!(text.includes('member')) && sibling.style){
             sibling.style.display='none'
-            // console.log();
         }
         sibling = sibling.nextSibling;
     }
-   
-    console.log(parentElement);
-    // console.log(signOutSpan);
 }
 
 modifyAccountInfo()
 
 const modifyPaymentDrop=async()=>{
-    let paymentDueLi=(await loadSelector('li > a[href="./member-payments"]')).parentNode
-    let parentUl=paymentDueLi.parentNode
-
-    let sibling  = parentUl.firstChild;
-    let toRemove=[]
-
-    while (sibling) {
-        let text=sibling.textContent.trim().toLowerCase()
-        // console.log(text);
-        if(text.includes('deposits') || text.includes('funds')  || text.includes('options')){
-            sibling.style.display='none'
-            toRemove.push(sibling)
-            // console.log(sibling);
-        }
-        sibling = sibling.nextSibling;
+    let paymentLis=$('li > a[href*="member-payments"]')
+   
+    let paymentOptLi=$('li > a[href*="Payment"]')
+   
+    if(paymentOptLi[0]){
+        paymentOptLi.each(function(index, element) {
+            let parent=$(element).parent()
+            let text=parent[0].innerText.trim()
+            if(text=='Payment Options'){
+                parent[0].style.display='none'
+            }
+        }); 
     }
-
-    toRemove.forEach(element => {
-        element.parentNode.removeChild(element);
-    });
-    console.log(parentUl);
+    if(paymentLis[0]){
+        paymentLis.each(function(index, element) {
+            let parent=$(element).parent()
+            let text=parent[0].innerText.trim()
+            
+            if(text=='Deposits' || text=='Funds' || text=='Payment Options'){
+                parent[0].style.display='none'
+            }
+        });
+    }
 }
 
-modifyPaymentDrop()
+var observer = new MutationObserver((mutations)=> {
+    mutations.forEach(mutation=>{
+        if(mutation.addedNodes.length!==0 && mutation.type === "childList"){
+            mutation.addedNodes.forEach(item=>{
+                modifyPaymentDrop()
+                
+                if(item.nodeName=='UL'){
+                   
+                }
+            
+            })
+        }
+    })
+  });
+observer.observe(document, { childList: true, subtree: true });
+
+
+const modifyLotsWonTable=async()=>{
+    let deliveryColumn=await loadSelector('th')
+    console.log(deliveryColumn);
+    // deliveryColumn.style.display='none'
+}
+if(window.location.href.includes('/lotsWon/')){
+    // modifyLotsWonTable()
+}
